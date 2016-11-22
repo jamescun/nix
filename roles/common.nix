2ad144363ec399c46f0f33092964ca4ca2a6924d
James Cunningham <me@jamescun.com>
2016-11-22 00:22:21 +0000

    add ssh configuration

--- roles/common.nix
+++ roles/common.nix
@@ -8,5 +8,19 @@
 
 	services = {
 		ntp.enable = true;
+
+		openssh = {
+			enable = true;
+
+			forwardX11 = false;
+			permitRootLogin = "no";
+			passwordAuthentication = false;
+		};
+	};
+
+	programs = {
+		ssh = {
+			startAgent = true;
+		};
 	};
 }
